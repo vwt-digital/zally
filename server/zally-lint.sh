163ff305e1ffa76cba783caa0c823847a72ea2e7
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-02-13 14:29:04 +0100

    Changed stups to jdk-slim DAT-1042

--- server/zally-lint.sh
+++ server/zally-lint.sh
@@ -2,17 +2,17 @@
 
 set -eo pipefail
 echo "checking argument..."
-if [ "$1" == "" ]; then
-    echo "argument not optional: spec file"
-    exit 1
+regcheck=".*yaml.*|.*json.*"
+if [[ "$1" =~ $regcheck ]]; then
+  echo "starting server..."
+  regcheck=".*Started ApplicationKt.*"
+  while IFS='' read -r line; do
+    [[ $line =~ $regcheck ]] || continue
+    echo "server seems to have started. beginning linting process..."
+    zally lint "$1"
+  done < <(java -jar /usr/local/bin/zallyserver.jar)
+  echo "server isn't starting correctly..."
+  exit 1
 fi
-echo "starting server..."
-regcheck=".*Started ApplicationKt.*"
-while IFS='' read -r line
-do
-  [[ $line =~ $regcheck ]] || continue
-  echo "server seems to have started. beginning linting process..."
-  zally lint "$1"
-done < <(java -jar /usr/local/bin/zallyserver.jar)
-echo "server isn't starting correctly..."
+echo "argument not optional: json or yaml spec file"
 exit 1
