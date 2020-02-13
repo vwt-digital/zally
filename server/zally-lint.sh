8fa3a27c11d9b9a73ee267f357842769d55c3826
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-02-13 09:21:10 +0100

    Linter now waits until server is ready DAT-1865

--- server/zally-lint.sh
+++ server/zally-lint.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 set -eo pipefail
-echo "checking argument"
-ls -l
+echo "checking argument..."
 if [ "$1" == "" ]; then
     echo "argument not optional: spec file"
     exit 1
 fi
-echo "starting linting process"
-java -jar /usr/local/bin/zallyserver.jar & sleep 80; zally lint "$1"
-# TODO - Use something else to wait for the server^
+echo "starting server..."
+regcheck=".*Started ApplicationKt.*"
+while IFS='' read -r line
+do
+  [[ $line =~ $regcheck ]] || continue
+  echo "server seems to have started. beginning linting process..."
+  zally lint "$1"
+done < <(java -jar /usr/local/bin/zallyserver.jar)
+echo "server isn't starting correctly..."
+exit 1
