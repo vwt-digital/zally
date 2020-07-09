15074e3fb62c8e011884371bdade9dfff520c7f9
Max van der Weide <max.vanderweide@student.hu.nl>
2020-07-09 12:01:23 +0200

    Added port and branch check DAT-3583

--- server/zally-lint.sh
+++ server/zally-lint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 set -eo pipefail
+
+if [ "$BRANCH_NAME" == "master" ]; then
+  echo "Skipping Zally in master branch."
+  exit 0
+fi
+
 echo "checking argument..."
 
 regcheck=".*yaml.*|.*json.*"
@@ -17,25 +23,26 @@ if [[ "$1" =~ $regcheck ]]; then
     echo "server seems to have started. beginning linting process..."
 
     attempt_counter=0
+    port=8000
 
-    until netcat -z -w 1 localhost 7979; do
-        if [ ${attempt_counter} -eq 5 ];then
-          echo "max attempts reached to connect with server. failing..."
-          exit 1
-        fi
+    until lsof -i -P -n | grep ".*:${port}" >/dev/null; do
+      if [ ${attempt_counter} -eq 5 ]; then
+        echo "max attempts reached to connect with server. failing..."
+        exit 1
+      fi
 
-        printf 'port 7979 not in use, trying again...\n'
-        attempt_counter=$((attempt_counter+1))
-        sleep 4
+      printf "port %s not in use, trying again...\n" "${port}"
+      attempt_counter=$((attempt_counter + 1))
+      sleep 4
     done
 
-    sleep 2; zally lint "$1"; exit 0
-
+    sleep 2
+    zally lint "$1"
+    exit 0
   done < <(java -jar /usr/local/bin/zallyserver.jar)
 
   echo "server isn't starting correctly..."
   exit 1
-
 fi
 
 echo "argument not optional: json or yaml spec file"
