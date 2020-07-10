a2b348602760fadf42ecfca954eefe9c7d48b30c
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-07-10 10:30:17 +0200

    Added memory min and max to Java runtime environment DAT-3583

--- server/zally-lint.sh
+++ server/zally-lint.sh
@@ -12,43 +12,34 @@ echo "checking argument..."
 regcheck=".*yaml.*|.*json.*"
 
 if [[ "$1" =~ $regcheck ]]; then
+  echo "starting server..."
+  regcheck=".*Started ApplicationKt.*"
 
-  java -jar /usr/local/bin/zallyserver.jar
-  echo "LSOF"
-  lsof -i -P -n
-  sleep 45
-  lsof -i -P -n
+  while IFS='' read -r line; do
 
-#  echo "starting server..."
-#  regcheck=".*Started ApplicationKt.*"
-#
-#  while IFS='' read -r line; do
-#
-#    echo "$line"
-#    [[ $line =~ $regcheck ]] || continue
-#    echo "server seems to have started. beginning linting process..."
-#
-#    attempt_counter=0
-#    port=8000
-#
-#    until lsof -i -P -n | grep ".*:${port}" >/dev/null; do
-#      if [ ${attempt_counter} -eq 5 ]; then
-#        echo "max attempts reached to connect with server. failing..."
-#        exit 1
-#      fi
-#
-#      printf "port %s not in use, trying again...\n" "${port}"
-#      attempt_counter=$((attempt_counter + 1))
-#      sleep 4
-#    done
-#
-#    sleep 4
-#    zally lint "$1"
-#    exit 0
-#  done < <(java -jar /usr/local/bin/zallyserver.jar)
-#
-#  echo "server isn't starting correctly..."
-#  exit 1
+    echo "$line"
+    [[ $line =~ $regcheck ]] || continue
+    echo "server seems to have started. beginning linting process..."
+
+    attempt_counter=0
+    port=8080
+
+    until lsof -i -P -n | grep ".*:${port}" >/dev/null; do
+      if [ ${attempt_counter} -eq 5 ]; then
+        echo "max attempts reached to connect with server. failing..."
+        exit 1
+      fi
+
+      printf "port %s not in use, trying again...\n" "${port}"
+      attempt_counter=$((attempt_counter + 1))
+      sleep 4
+    done
+
+    sleep 4; zally lint "$1"; exit 0
+  done < <(java -Xms512m -Xmx1024m -jar /usr/local/bin/zallyserver.jar)
+
+  echo "server isn't starting correctly..."
+  exit 1
 fi
 
 echo "argument not optional: json or yaml spec file"
